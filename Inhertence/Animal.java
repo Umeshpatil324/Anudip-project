c6dd88c2b369b64ca34f595e8185cac23f7522ae
Umesh Ishwar Patil <uc2073593@gmail.com>
2026-01-23 23:49:34 +0530

    Add files via upload

---
+++ Inhertence/Animal.java
@@ -0,0 +1,9 @@
+package Inhertence;
+
+public class Animal {
+	public void eat() {
+		System.out.println("animal eat");
+		
+	}
+
+}
---
+++ Inhertence/Dog.java
@@ -0,0 +1,13 @@
+package Inhertence;
+
+public class Dog extends Animal {
+	public static void main(String[] args) {
+		
+	
+	Dog d = new Dog();
+	d.eat();
+	
+	
+	}
+
+}
