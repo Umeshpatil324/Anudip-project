a5cb711b83271e5a1bbbc25006ab4708806c7835
Umesh Ishwar Patil <uc2073593@gmail.com>
2026-01-24 23:28:58 +0530

    Add files via upload

---
+++ Constructor/College.java
@@ -0,0 +1,14 @@
+package Constructor;
+
+public class College {
+	public static void main(String[] args) {
+		Student n = new Student();
+		
+		n.name = "umesgrthg";
+		n.age =52;
+		
+		System.out.println(n.name);
+		System.out.println(n.age);
+	}
+
+}
---
+++ Constructor/Student.java
@@ -0,0 +1,13 @@
+package Constructor;
+
+public class Student {
+	String name;
+	int age;
+		Student(){
+		
+	}
+	Student(String name, int age){
+		this.name =name;
+		this.age = age;
+	}
+}
