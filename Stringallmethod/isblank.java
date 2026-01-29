f3d942f90506d881ab651489949b5b983bc9ae5d
Umesh Ishwar Patil <uc2073593@gmail.com>
2026-01-29 23:51:45 +0530

    Add files via upload

---
+++ Stringallmethod/isblank.java
@@ -0,0 +1,15 @@
+package Stringallmethod;
+
+public class isblank {
+	public static void main(String[] args) {
+		String str = "java";
+		String str1 = "   dfnsjn";
+		
+		System.out.println(str.split("a"));
+		//System.out.println(str.substring(0, 4));//start to end of index
+		//System.out.println(str.isBlank());//when the blank space or emty in
+		//string then give true otherwise give false
+	
+	}
+
+}
